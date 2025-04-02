9b90dda36549ccf3bc45bec2e2c5d4200668dd86
KerissaGit <kerissa525@gmail.com>
2025-04-02 13:48:51 -0400

    Passes all tests

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        response_body = jsonify([message.to_dict() for message in messages])
+        return make_response(response_body, 200)
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body = data.get('body'),
+            username = data.get('username')
+        )
 
-@app.route('/messages/<int:id>')
+        db.session.add(new_message)
+        db.session.commit()
+
+        response_body = jsonify(new_message.to_dict())
+        return make_response(response_body, 201)
+
+@app.route('/messages/<int:id>', methods = ['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if not message:
+        return jsonify({'Error': 'Message not found'}), 404
+    
+    if request.method == 'PATCH':
+        data = request.get_json()
+        if 'body' not in data:
+            return make_response(jsonify({'Error': 'Message not found'}), 404)
+        message.body = data['body']
+        db.session.commit()
+        response_body = jsonify(message.to_dict())
+        return make_response(response_body, 200)
+        
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({'Message': 'Message deleted successfully'}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
